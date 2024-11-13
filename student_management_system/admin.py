d288f91972e315a659bb1811a5c808eb3684abdc
Kirin-Hunter-Nan <jiaqinan705@gmail.com>
2024-11-13 23:41:55 +0800

    change the data_operation

--- student_management_system/admin.py
+++ student_management_system/admin.py
@@ -7,3 +7,9 @@ admin_instance = Admin()
 def admin_window(admin_id):
     print(f"-------------------welcome {admin_id} -------------------")
     print(admin_instance.show_information(admin_id))
+
+    print("Please choose your choice")
+
+### 根据选择，进入不同的系统
+# 1学生，2老师，
+# 选择1， 对学生进行处理，选择2， 对教师进行处理
--- student_management_system/database_operation.py
+++ student_management_system/database_operation.py
@@ -18,7 +18,9 @@ class Student:
         """
 
         get_password = """
+        
         select stu_password from student where stu_id = %S"""
+
         self.cursor.execute(get_password, stu_id)
         result: tuple = cursor.fetchone()
         if result is not None:
@@ -31,7 +33,9 @@ class Student:
         """
 
         show_information = """
+        
         select stu_id, stu_name, stu_phone from student where stu_id = %s"""
+
         self.cursor.execute(show_information, stu_id)
         result: tuple = cursor.fetchall()
         for r in result:
@@ -48,7 +52,9 @@ class Teacher:
         """
 
         get_password = """
+        
         select tea_password from teacher where tea_id = %s """
+
         self.cursor.execute(get_password, tea_id)
         result: tuple = cursor.fetchone()
         if result is not None:
@@ -61,7 +67,9 @@ class Teacher:
         """
 
         show_information = """
+        
         select tea_id, tea_name, tea_phone, tea_class from teacher where tea_id = %s"""
+
         self.cursor.execute(show_information, tea_id)
         result: tuple = cursor.fetchall()
         for r in result:
@@ -77,7 +85,9 @@ class Admin:
         get the password of admin and compared it in main.py for admin logging in
         """
         get_password = """
+        
         select admin_password from admin where admin_id = %s"""
+
         self.cursor.execute(get_password, admin_id)
         result: tuple = cursor.fetchone()
         if result is not None:
@@ -87,42 +97,201 @@ class Admin:
     def show_information(self, admin_id):
         """
         show the information of the admin
-        :return:
         """
         show_information = """
+        
         select admin_id, admin_name, admin_phone from admin where admin_id = %s"""
+
         self.cursor.execute(show_information, admin_id)
         result: tuple = cursor.fetchall()
         for r in result:
             return r
 
+    def show_student_information(self, id):
+        """
+        show the information of a student
+        :return:
+        """
+
+        show_information = """
+        
+        select stu_id, stu_name, stu_phone, stu_class from student where stu_id = %s"""
+
+        self.cursor.execute(show_information, id)
+
+    def show_teacher_information(self, id):
+        """
+        show the information of a teacher
+        :param id:
+        :return:
+        """
+
+        show_information = """
+        
+        select tea_id, tea_name, tea_phone, tea_class from teacher where tea_id = %s"""
+
+        self.cursor.execute(show_information, id)
+
     def insert_student_information(self, values):
         """
         insert the information of a student
-        :return:
         """
         insert_information = """
+        
         insert into student (id, stu_id, stu_name, stu_phone, stu_class, stu_password) values (NULL, %s, %s, %s, %s, %s)"""
+
         values = values.split(',')
         self.cursor.execute(insert_information, values)
 
     def insert_teacher_information(self, values):
         """
         insert the information of a teacher
-        :return:
         """
         insert_information = """
+        
         insert into teacher (id, tea_id, tea_name, tea_phone, tea_class, tea_password) values (NULL, %s, %s, %s, %s ,%s)"""
+
         values = values.split(',')
         self.cursor.execute(insert_information, values)
 
-    # def update_student_information(self, stu_id, stu_choice):
-    #     """
-    #     update the information of a student
-    #     :return:
-    #     """
+    def update_student_information(self, choice, stu_id, new_information, ):
+        """
+        update the information of a student
+        :param choice:
+        :param stu_id:
+        :param new_information
+        """
 
+        # update the student name in the database
+        if choice == "1 ":
+            update_information = """
+            
+            update student set stu_name = %s where stu_id = %s"""
 
+            values = (new_information, stu_id)
+            self.cursor.execute(update_information, values)
+
+        # update the student iphone in the database
+        if choice == "2":
+            update_information = """
+            
+            update student set stu_phone = %s where stu_id = %s"""
+
+            values = (new_information, stu_id)
+            self.cursor.execute(update_information, values)
+
+        # update the student class in the database
+        if choice == "3":
+            update_information = """
+            
+            update student set stu_class = %s where stu_id = %s"""
+
+            values = (new_information, stu_id)
+            self.cursor.execute(update_information, values)
+
+        # update the student password in the database
+        if choice == "4":
+            update_information = """
+            
+            update student set stu_password = %s where stu_id = %s"""
+
+            values = (new_information, stu_id)
+            self.cursor.execute(update_information, values)
+
+    def update_teacher_information(self, choice, new_information, tea_id):
+        """
+        update the information of a teacher
+        :param choice:
+        :param new_information:
+        :param tea_id:
+        :return:
+        """
+
+        if choice == "1":
+            update_information = """
+            
+            update teacher set tea_name = %s where tea_id = %s"""
+
+            values = (new_information, tea_id)
+            self.cursor.execute(update_information, values)
+
+        if choice == "2":
+            update_information = """
+            
+            update teacher set tea_phone = %s where tea_id = %s"""
+
+            values = (new_information, tea_id)
+            self.cursor.execute(update_information, values)
+
+        if choice == "3":
+            update_information = """
+            
+            update teacher set tea_class = %s where tea_id = %s"""
+
+            values = (new_information, tea_id)
+            self.cursor.execute(update_information, values)
+
+        if choice == "4":
+            update_information = """
+            
+            update teacher set tea_password = %s where tea_id = %s"""
+
+            values = (new_information, tea_id)
+            self.cursor.execute(update_information, values)
+
+    def update_admin_information(self, choice, new_information, admin_id):
+        """
+        update the information of a admin
+        """
+
+        if choice == "1":
+            update_information = """
+            
+            update admin set admin_name = %s where admin_id = %s"""
+
+            values = (new_information, admin_id)
+            self.cursor.execute(update_information, values)
+
+        if choice == "2":
+            update_information = """
+            
+            update admin set admin_phone = %s where admin_id = %s"""
+
+            values = (new_information, admin_id)
+            self.cursor.execute(update_information, values)
+
+        if choice == "3":
+            update_information = """
+            
+            update admin set admin_password = %s where admin_id = %s"""
+
+            values = (new_information, admin_id)
+            self.cursor.execute(update_information, values)
+
+    def delete_information(self, choice, id):
+        """
+        if choice == 1
+        it will delete the information of a student
+
+        elif choice == 2
+        it will delete the information of a teacher
+        :param choice:
+        :param id:
+        :return:
+        """
+        if choice == "1":
+            delete_information = """
+            
+            delete from student where stu_id = %s"""
+
+            self.cursor.execute(delete_information, id)
+
+        if choice == "2":
+            delete_information = """
+            
+            delete from teacher where tea_id = %s"""
+
+            self.cursor.execute(delete_information, id)
 
 
 # insert_information()
