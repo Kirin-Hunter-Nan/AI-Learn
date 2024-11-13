589e123f49ed0420b3a2443500e91b5f5e5a1db1
Kirin-Hunter-Nan <jiaqinan705@gmail.com>
2024-11-13 17:06:33 +0800

    add the information of student.py

--- student_management_system/student.py
+++ student_management_system/student.py
@@ -1,5 +1,17 @@
 import database_operation
 
+
 def student_window(student_id):
     print(f"-------------------welcome {student_id} -------------------")
     print(database_operation.show_stu_information(student_id))
+
+    print("please choose your choice:")
+    choice = input()
+    if choice == "1":
+        print("进入学生选课系统")
+
+    if choice == "2":
+        print("进入成绩查询系统")
+
+    if choice == "3":
+        print("退学")
